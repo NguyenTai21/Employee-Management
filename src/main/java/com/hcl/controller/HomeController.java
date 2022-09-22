6285a7943c3057b19cf96cc5b3be5e90f51819ca
NguyenTai21 <nguyentai211299@gmail.com>
2022-09-22 19:37:05 +0700

    commit to main

--- src/main/java/com/hcl/controller/HomeController.java
+++ src/main/java/com/hcl/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.hcl.controller;
 
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
@@ -28,132 +27,134 @@ import com.hcl.service.EmployeeService;
 @Controller
 public class HomeController {
 
-	public static String userIn;
+	public static String userIn = "";
 	@Autowired
 	EmployeeService service;
-	
+
 	@Autowired
 	AccountService accservice;
-	
-	@RequestMapping(value="/home")
-	public ModelAndView home(@ModelAttribute("account") Account account,HttpServletRequest request) {
-		System.out.println("Session "+(String)request.getParameter("username"));
-		String usernameSession = (String)request.getParameter("username");
-		String username =  account.getUsername();
-		String password =  account.getPassword();
-		if(username == null) {
+
+	@RequestMapping(value = "/home")
+	public ModelAndView home(@ModelAttribute("account") Account account) {
+
+		String usernameSession = userIn;
+		String username = account.getUsername();
+		String password = account.getPassword();
+		if (username == null) {
 			username = "";
 		}
-		if(password == null) {
+		if (password == null) {
 			password = "";
 		}
-		System.out.println("U: "+username+" P: "+password);
-		if(!accservice.checkAccountExist(username) && !accservice.checkAccountExist(usernameSession) ) {
-
-			 if(username == "" || password == "") {
-				ModelAndView mav = new ModelAndView("login");
-				mav.addObject("message","Username or password can't be empty!!!");
-				
+		System.out.println("U: " + username + " P: " + password);
+		System.out.println("session " + usernameSession);
+		if (usernameSession != "") {
+			if (accservice.isAdmin(usernameSession)) {
+				ModelAndView mav = new ModelAndView();
+				mav.setViewName("home");
+				mav.addObject("username", username);
+				mav.addObject("password", password);
+				mav.addObject("empList", service.getEmployees());
+				return mav;
+			} else {
+				ModelAndView mav = new ModelAndView();
+				mav.setViewName("profile");
+				Account acc = accservice.findByUsername(username);
+				mav.addObject("employee", acc.getEmployee());
+				mav.addObject("username", username);
+				mav.addObject("password", password);
 				return mav;
 			}
+		}
+		if (username == "" || password == "") {
 			ModelAndView mav = new ModelAndView("login");
-			mav.addObject("message","Invalid username or password!!!");
-			
+			mav.addObject("message", "Username or password can't be empty!!!");
+			return mav;
+
+		} else if (accservice.checkLogin(username, password) == null) {
+			ModelAndView mav = new ModelAndView("login");
+			mav.addObject("message", "Invalid username or password!!!");
 			return mav;
 		}
-		
-			
-				
-		else if(accservice.checkLogin(username, password) == null && usernameSession == null) {
-			ModelAndView mav = new ModelAndView("login");
-			mav.addObject("message","Invalid username or password!!!");
-			
-			return mav;
-		}	
-		
-		
-			
-		else if(accservice.isAdmin(username) || accservice.isAdmin(usernameSession)) {
+
+		if ((accservice.isAdmin(username) && accservice.checkLogin(username, password) != null)) {
 			ModelAndView mav = new ModelAndView();
 			mav.setViewName("home");
-			HttpSession session = request.getSession(true);
-			session.setAttribute("username",username);
-			session.setAttribute("password",password);
+			mav.addObject("username", username);
+			mav.addObject("password", password);
 			mav.addObject("empList", service.getEmployees());
-			mav.addObject("session",session);
+			System.out.println("adminlogin");
 			userIn = username;
-			return mav;	
-		}else {
+			return mav;
+		} else if ((!accservice.isAdmin(username) && accservice.checkLogin(username, password) != null)) {
 			ModelAndView mav = new ModelAndView();
 			mav.setViewName("profile");
 			Account acc = accservice.findByUsername(username);
 			mav.addObject("employee", acc.getEmployee());
-			HttpSession session = request.getSession(true);
-			session.setAttribute("username",username);
-			session.setAttribute("password",password);
-			mav.addObject("session",session);
+			mav.addObject("username", username);
+			mav.addObject("password", password);
+			System.out.println("userlogin");
 			userIn = username;
 			return mav;
-			
+
+		} else {
+			return new ModelAndView("redirect:/");
 		}
-		
-		
+
 	}
-	
-	
+
 	@RequestMapping("/")
 	public ModelAndView backToLogin() {
 		ModelAndView mav = new ModelAndView("login");
 		Account account = new Account();
-		mav.addObject("account",account);
+		userIn = "";
+		mav.addObject("account", account);
 		return mav;
 	}
-	
+
 	@RequestMapping("/save")
-	public ModelAndView saveEmployee(@ModelAttribute("employee") Employee emp,HttpServletRequest request) {
+	public ModelAndView saveEmployee(@ModelAttribute("employee") Employee emp, HttpServletRequest request) {
 		ModelAndView mav = new ModelAndView();
 		System.out.println(emp.toString());
 		mav.setViewName("redirect:/home");
-		mav.addObject("username",userIn);
+		mav.addObject("username", userIn);
 		service.saveEmployee(emp);
 		return mav;
 	}
+
 	@RequestMapping(value = "/updateEmp/{eno}", method = RequestMethod.POST)
-	public ModelAndView updateEmployee(Employee emp, @PathVariable int eno,HttpServletRequest request) {
-		
+	public ModelAndView updateEmployee(Employee emp, @PathVariable int eno, HttpServletRequest request) {
+
 		ModelAndView mav = new ModelAndView("redirect:/home");
-		
+
 		System.out.println(emp.toString());
-		mav.addObject("username",userIn);
+		mav.addObject("username", userIn);
 		emp.setEno(eno);
 		System.out.println(service.saveEmployee(emp));
 		return mav;
 	}
 
-	
 	@RequestMapping("/edit")
 	public ModelAndView saveEmployee(@RequestParam int eno) {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("edit");
-		mav.addObject("employee",service.getEmpById(eno));
+		mav.addObject("employee", service.getEmpById(eno));
 		return mav;
 	}
-	
-	
+
 	@RequestMapping("/delete")
 	public ModelAndView deleteEmployee(@RequestParam int eno) {
 		ModelAndView mav = new ModelAndView("redirect:/home");
-		mav.addObject("username",userIn);
+		mav.addObject("username", userIn);
 		service.deleteEmployee(eno);
 		return mav;
 	}
-	
+
 	@RequestMapping("/create")
 	public ModelAndView createEmployee(@ModelAttribute("employee") Employee emp) {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("create");
 		return mav;
 	}
-	
-	
+
 }
